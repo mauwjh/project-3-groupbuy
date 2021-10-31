594d239f54d8f730af264890857546e346eaf75b
linxxnint <vasteple@gmail.com>
2021-10-31 16:09:40 +0800

    test out Nav and About page with routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,21 @@ import './App.css';
 import {Route, Switch} from 'react-router'
 import ListingNew from './Components/ListingNew';
 import ButtonAppBar from './Components/Navbar';
+import NaviBar from './Components/Navibar';
+import About from './Components/About';
 
 function App() {
   return (
     <>
-    <ButtonAppBar />
+      <ButtonAppBar />
+      <NaviBar/>
     <div className="App">
       <Switch>
         <Route path='/listing/new'>
           <ListingNew />
+          </Route>
+          <Route path='/listing/about'>
+          <About />
         </Route>
       </Switch>
     </div>
