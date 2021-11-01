83b40671a5031f36c33af6f0eaf4d60a61eb409e
mauwjh <matthew.auw@gmail.com>
2021-11-02 01:56:49 +0800

    added orders table for seller's listing view

---
+++ client/src/Components/OrdersTable.jsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+  { field:'id', hide: true},
+  { field: 'username', headerName: 'Username', minWidth: 200, flex: 1 },
+  { field: 'qty', headerName: 'Quantity Reserved', minWidth: 150, flex: 1 },
+  { field: 'address', headerName: 'Address', minWidth: 400, flex: 3 },
+];
+
+
+export default function OrdersTable({ordersData}) {
+
+  const rows = 
+    ordersData?.map((a,b) => ({id: b+1, username: a.buyer_id[0].username, qty: a.qty_reserved, address:a.buyer_id[0].address}))
+  
+
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        getRowId={(row) => row.id}
+        disableSelectionOnClick
+      />
+    </div>
+  );
+}
