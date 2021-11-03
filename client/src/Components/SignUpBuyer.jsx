3ed167a87c0de45629b0c38abfeef2566583667c
linxxnint <vasteple@gmail.com>
2021-11-03 22:57:49 +0800

    routed to login page after create seller or buyer

--- client/src/Components/SignUpBuyer.jsx
+++ client/src/Components/SignUpBuyer.jsx
@@ -3,7 +3,6 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
@@ -11,10 +10,15 @@ import SetMeal from "@mui/icons-material/SetMeal";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import axios from "axios";
+import { useHistory } from "react-router-dom";
+
 
 const theme = createTheme();
 
 export default function SignUpBuyer() {
+  let history = useHistory();
+
+  
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -40,6 +44,7 @@ export default function SignUpBuyer() {
       })
       .then((response) => {
         console.log(response);
+        history.push("/login"); //or create listing? 
       });
   };
 
@@ -94,7 +99,7 @@ export default function SignUpBuyer() {
             </div>
             <Box
               component="form"
-              noValidate
+              Validate
               onSubmit={handleSubmit}
               sx={{ mt: 1 }}
             >
@@ -119,6 +124,8 @@ export default function SignUpBuyer() {
                 label="Key In Your Name"
                 name="username"
                 autoFocus
+                error
+                helperText="Please complete"
               />
               <TextField
                 margin="normal"
