df5f05cd113f6fc472f3d5e98d6cd4b112bfe0d0
linxxnint <vasteple@gmail.com>
2021-11-04 01:06:52 +0800

    edited alert to modal popup if wrong email or pw

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -14,9 +14,9 @@ import { createTheme, ThemeProvider } from "@mui/material/styles";
 import axios from "axios";
 import AuthApi from "../Utility/AuthApi";
 import { useHistory } from "react-router-dom";
+import { Modal } from "@material-ui/core";
 
 function Copyright(props) {
-
   return (
     <Typography
       variant="body2"
@@ -37,8 +37,31 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function Login() {
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  function myFunction() {
+    //document.getElementById("para").style.color = "red";
+    //document.getElementById("para").innerHTML =
+     // "'You have keyed in an Invalid UserName or PassWord'";
+   handleOpen(); // this is for modal
+  }
   const authApi = useContext(AuthApi);
- 
+
   let history = useHistory();
 
   const handleSubmit = (event) => {
@@ -51,21 +74,25 @@ export default function Login() {
         email: data.get("email"),
         password: data.get("password"),
       })
-      .then((response) => {
-        // console.log("Response Data", response.data);
-        console.log("UserInfo", response.data);
-        if (response.data.auth) {
-          console.log("Am I logged in?", response.data.message, response.data.userInfo._doc);
-          authApi.setAuth({
-            session: response.data.auth,
-            userInfo: response.data.userInfo._doc,
-          });
-          history.push("/");
-         
-          }
-          else {
-          console.log(response.data.message)
-          alert('You have keyed in an Invalid UserName or PassWord');
+      .then(
+        (response) => {
+          // console.log("Response Data", response.data);
+          console.log("UserInfo", response.data);
+          if (response.data.auth) {
+            console.log(
+              "Am I logged in?",
+              response.data.message,
+              response.data.userInfo._doc
+            );
+            authApi.setAuth({
+              session: response.data.auth,
+              userInfo: response.data.userInfo._doc,
+            });
+            history.push("/");
+          } else {
+            console.log(response.data.message);
+            myFunction();
+            //alert("You have keyed in an Invalid UserName or PassWord");
           }
         },
         (error) => {
@@ -73,12 +100,6 @@ export default function Login() {
         }
       );
   };
-
- 
-  //function timedtest() {
-  setTimeout("location.href = " / ";", 1500);
-  
-  
 
   return (
     <ThemeProvider theme={theme}>
@@ -98,11 +119,7 @@ export default function Login() {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            sx={{ mt: 1 }}
-          >
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
             <TextField
               margin="normal"
               required
@@ -129,7 +146,6 @@ export default function Login() {
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
-              //onClick= {timedtest}
             >
               Sign In
             </Button>
@@ -140,9 +156,24 @@ export default function Login() {
                 </Link>
               </Grid>
             </Grid>
+           {/* <p id="para">Please login</p> */}
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                 I am a Modal
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2, color: "red"  }}>
+                 You have keyed in the wrong email add or password !!!
+                </Typography>
+              </Box>
+            </Modal>
           </Box>
         </Box>
-    
         <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
     </ThemeProvider>
