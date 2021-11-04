447ffe07488f884faef30adc629c2005a5556962
mauwjh <matthew.auw@gmail.com>
2021-11-04 22:07:04 +0800

    debugged new listing component

--- client/src/Components/ListingNew.jsx
+++ client/src/Components/ListingNew.jsx
@@ -11,7 +11,6 @@ import axios from "axios";
 import {useHistory} from 'react-router-dom'
 import AuthApi from "../Utility/AuthApi";
 import Typography from "@mui/material/Typography";
-import { useHistory } from "react-router";
 
 // * TODO Add number of days between start and end date
 
@@ -26,7 +25,6 @@ const ListingNew = () => {
   const [file, setFile] = useState();
   const history = useHistory()
   const session = useContext(AuthApi);
-  let history = useHistory()
 
   const createNewListing = async (listingData) => {
     console.log(listingData)
